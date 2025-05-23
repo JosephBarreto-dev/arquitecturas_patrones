22561a19c7bacc7a87d89106e02861bff96ab0d2
KristianMR <kmogrovejo@ucundinamarca.edu.co>
2025-05-23 15:36:09 -0500

    Patrón Adapter

---
+++ Patrones_Estructurales/PatronesEstructurales/src/Adapter/ExternalPaymentAPI.java
@@ -0,0 +1,7 @@
+package Adapter;
+
+public class ExternalPaymentAPI {
+    public void realizarPago(double cantidad) {
+        System.out.println("Pago realizado de $" + cantidad);
+    }
+}
---
+++ Patrones_Estructurales/PatronesEstructurales/src/Adapter/Pago.java
@@ -0,0 +1,5 @@
+package Adapter;
+
+public interface Pago {
+    void pagar(double cantidad);
+}
